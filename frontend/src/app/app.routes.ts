f8cd9ff1594f67267051764d8c2ba9a49155c7a5
Mostafa Mahmoud Fawzy <m3asbhomelkeber2@gmail.com>
2026-09-17 01:30:13 +0300

    feat(frontend): add initial Cartify routing

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
+import { Home } from './pages/home/home';
+import { Products } from './pages/products/products';
+import { ProductDetails } from './pages/product-details/product-details';
+import { Login } from './pages/login/login';
+import { Signup } from './pages/signup/signup';
+import { Cart } from './pages/cart/cart';
+import { NotFound } from './pages/not-found/not-found';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'products', component: Products },
+  { path: 'products/:id', component: ProductDetails },
+  { path: 'login', component: Login },
+  { path: 'signup', component: Signup },
+  { path: 'cart', component: Cart, canActivate: [authGuard] },
+  { path: '**', component: NotFound }
+];
